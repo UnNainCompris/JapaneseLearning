5d43cacdd8aa445f59f53a756c398fbd1f590ef1
UnNainCompris <79692590+UnNainCompris@users.noreply.github.com>
2024-07-27 22:40:38 +0200

    Add string utils

---
+++ string_utils.c
@@ -0,0 +1,265 @@
+#include "string_utils.h"
+
+int strlength(const char* string) {
+    if (string == NULL) {
+        return 0;
+    }
+
+    int size = 0;
+    while (string[size]) {
+        size++;
+    }
+    return size;
+}
+
+char* copy(const char* string) {
+    if (string == NULL) {
+        return NULL;
+    }
+
+    int string_length = strlength(string);
+    char* copy = calloc(string_length, sizeof(char));
+    for (int index = 0 ; index < string_length ; index++) {
+        copy[index] = string[index];
+    }
+    return copy;
+}
+
+char* substring(const char* string, int start, int end) {
+    const int string_length = strlength(string);
+
+    start = min(max(start, 0), string_length);
+    end = min(max(end, 0), string_length);
+
+    if (start == end) {
+        return " ";
+    }
+
+    if (start >= end) {
+        return NULL;
+    }
+
+    char* result = calloc(end - start, sizeof(char));
+
+    for (int index = start ; index < end ; index++) {
+        if (string[index]) {
+            result[index - start] = string[index];
+        } else {
+            break;
+        }
+    }
+
+    return result;
+}
+
+int count_matching_row_char(const char* string, const char* model) {
+    if (string == NULL || model == NULL) {
+        return 0;
+    }
+
+    int matching = 0, model_length = strlength(model);
+    while (matching < model_length && string[matching] && model[matching]) {
+        if (string[matching] == model[matching]) {
+            matching++;
+        } else {
+            break;
+        }
+    }
+
+    return matching;
+}
+
+int count_matching_char(const char* string, const char* model) {
+    int matching = 0, model_length = strlength(model);
+    while (matching < model_length && string[model_length] && model[model_length]) {
+        if (string[matching] == model[matching]) {
+            matching++;
+        } else {
+            break;
+        }
+    }
+
+    return matching;
+}
+
+int* find_index(const char* string, const char* model, int* amount_of_index) {
+    if (string == NULL || model == NULL) {
+        return NULL;
+    }
+
+    int should_free_index_amount = 0;
+    if (amount_of_index == NULL) {
+        amount_of_index = malloc(sizeof(int));
+        should_free_index_amount = 1;
+    }
+
+    int model_length = strlength(model);
+    (*amount_of_index) = count(string, model);
+    int* buffer = calloc(*amount_of_index, sizeof(int));
+    int buffer_index = 0;
+
+    for (int index = 0 ; index < strlength(string) ; index++) {
+        if (string[index] == model[0]) {
+            char* sub = substring(string, index, index + model_length);
+            if (start_with(sub, model)) {
+                buffer[buffer_index] = index;
+                index += model_length - 1;
+                buffer_index++;
+            }
+            free(sub);
+        }
+    }
+
+    if (should_free_index_amount) {
+        free(amount_of_index);
+    }
+
+    return buffer;
+}
+
+
+int start_with(const char* string, const char* model) {
+    if (string == NULL || model == NULL) {
+        return 0;
+    }
+
+    for (int i = 0 ; i < strlength(model) ; i++) {
+        if (string[i] != model[i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int count(const char* string, const char* model) {
+    int found = 0;
+    int model_length = strlength(model);
+
+    for (int index = 0 ; index < strlength(string) ; index++) {
+        if (string[index] == model[0]) {
+            char* sub = substring(string, index, index + model_length);
+            if (start_with(sub, model)) {
+                found++;
+                index += model_length - 1;
+            }
+            free(sub);
+        }
+    }
+
+    return found;
+}
+
+char* replace(const char* string, const char* to_replace, const char* replacement, int limit) {
+    int string_length = strlength(string);
+    int to_replace_length = strlength(to_replace);
+    int replacement_length = strlength(replacement);
+
+    int max_length = max(to_replace_length, replacement_length) * string_length;
+    char* buffer = calloc(max_length, sizeof(char));
+    int active_buffer_size = 0;
+
+    int amount_of_replace = 0;
+
+    for (int i = 0 ; i < string_length ; i++) {
+        char* sub = substring(string, i, to_replace_length + i);
+        if (count_matching_row_char(sub, to_replace) == to_replace_length && amount_of_replace != limit) {
+            for (int replacement_index = 0 ; replacement_index < replacement_length ; replacement_index++) {
+                buffer[active_buffer_size] = replacement[replacement_index];
+                active_buffer_size++;
+            }
+            amount_of_replace++;
+            i += to_replace_length - 1;
+        } else {
+            buffer[active_buffer_size] = string[i];
+            active_buffer_size++;
+        }
+        free(sub);
+    }
+    char* replaced_string = copy(buffer);
+    free(buffer);
+    return replaced_string;
+}
+
+char* replace_first(const char* string, const char* to_replace, const char* replacement) {
+    return replace(string, to_replace, replacement, 1);
+}
+
+char* replace_from_end(const char* string, const char* to_replace, const char* replacement, int limit) {
+    int string_length = strlength(string);
+    int to_replace_length = strlength(to_replace);
+    int replacement_length = strlength(replacement);
+
+    int max_length = max(to_replace_length, replacement_length) * string_length;
+    char* buffer = calloc(max_length, sizeof(char));
+    int active_buffer_size = 0;
+
+    int max_amount_of_replace = count(string, to_replace);
+    int current_replace_index = 0;
+
+    for (int i = 0 ; i < string_length ; i++) {
+        char* sub = substring(string, i, to_replace_length + i);
+        if (count_matching_row_char(sub, to_replace) == to_replace_length) {
+            if (max_amount_of_replace - current_replace_index <= limit) {
+                for (int replacement_index = 0; replacement_index < replacement_length; replacement_index++) {
+                    buffer[active_buffer_size] = replacement[replacement_index];
+                    active_buffer_size++;
+                }
+            } else {
+                for (int replacement_index = 0; replacement_index < to_replace_length; replacement_index++) {
+                    buffer[active_buffer_size] = to_replace[replacement_index];
+                    active_buffer_size++;
+                }
+            }
+
+            i += to_replace_length - 1;
+            current_replace_index++;
+        } else {
+            buffer[active_buffer_size] = string[i];
+            active_buffer_size++;
+        }
+        free(sub);
+    }
+    char* replaced_string = copy(buffer);
+    free(buffer);
+    return replaced_string;
+}
+
+char* replace_last(const char* string, const char* to_replace, const char* replacement) {
+    return replace_from_end(string, to_replace, replacement, 1);
+}
+
+char** split(const char* string, const char* splitter, int limit, int* string_amount) {
+    int split_counter = 0;
+    int max_split = count(string, splitter);
+    int required_split_amount = (limit <= 0 ? max_split : limit - 1);
+    int* index_list = find_index(string, splitter, NULL);
+
+    char** buffer = calloc(required_split_amount, sizeof(char*));
+    int buffer_index = 0;
+
+    while (split_counter < required_split_amount) {
+        int substring_start = index_list[split_counter] + strlength(splitter);
+        int substring_end = index_list[split_counter + 1];
+
+        if(split_counter + 1 == required_split_amount) {
+            substring_end = strlength(string);
+        }
+
+        buffer[buffer_index] = substring(string, substring_start, substring_end);
+        buffer_index++;
+        split_counter++;
+    }
+
+    if (split_counter < max_split) {
+        int substring_start = index_list[split_counter] + strlength(splitter);
+        int substring_end = strlength(string);
+        buffer[buffer_index] = substring(string, substring_start, substring_end);
+    }
+
+    if (string_amount != NULL) {
+        (*string_amount) = buffer_index;
+    }
+
+    free(index_list);
+    return buffer;
+}
---
+++ string_utils.h
@@ -0,0 +1,159 @@
+#ifndef ERIS_STRING_UTILS_H
+#define ERIS_STRING_UTILS_H
+
+#include <stdlib.h> // From memory management.
+#include <stdio.h> // Debugging purpose only
+
+#define max(a, b) (a > b ? a : b)
+#define min(a, b) (a > b ? b : a)
+
+/**
+ * Use to retrieve the length of a string.
+ * @param string The string that we want to check his length.
+ * @return The founded length of 'string'.
+ */
+int strlength(const char* string);
+
+/**
+ * Use to copy a string (char array) inside a new one.
+ * @depend_on strlength(), calloc()
+ * @param string The string to copy.
+ * @return A copy of the string inside a new array.
+ */
+char* copy(const char* string);
+
+/**
+ * Use to create a substring of a string <br>
+ * <br>
+ * 'start' & 'end' min is automatically capped to 0 <br>
+ * 'start' & 'end' max is automatically capped to string length <br>
+ * <br>
+ * If 'start' greater than 'end' this function return NULL <br>
+ * If 'start' is equals to 'end' this function return an empty string ("") <br>
+ * If 'start' is smaller than 'end' this function will work like so: <br>
+ * <br>
+ * substring("Hamburger", 0, 5); -> "Hambu" <br>
+ * substring("Hamburger", 2, 7); -> "mburg" <br>
+ * substring("Hamburger", 5, 9); -> "rger" <br>
+ * substring("Hamburger", 3, 3); -> "" <br>
+ * substring("Hamburger", -10000, 10000); -> "Hamburger"
+ * @depend_on strlength(), max(), min(), calloc()
+ * @param string The string to work with
+ * @param start Index of where to start (INCLUSIVE)
+ * @param end Index of where to stop (EXCLUSIVE)
+ * @return The substring created and found from string with the start and end index
+ */
+char* substring(const char* string, int start, int end);
+
+/**
+ * Use to count how many char in a row are similar. <br>
+ * <br>
+ * ex: <br>
+ * string = "For D.den"; model = "Forbidden" <br>
+ * For D.den <br>
+ * Forbidden <br>
+ * ^^^XXX^^^ <br>
+ * This will return 3 The last 3 doesn't work because they aren't in row.
+ * @depend_on strlength()
+ * @param string The string where we are going to search for model
+ * @param model The model we are using to search in string
+ * @return The amount of char in a row that is similar from the start of string.
+ */
+int count_matching_row_char(const char* string, const char* model);
+
+/**
+ * Use to count how many char in a are similar. <br>
+ * <br>
+ * ex: <br>
+ * string = "Foo Bar"; model = "Honey" <br>
+ * Foo Bar <br>
+ * Honey <br>
+ * X^XXX <br>
+ * This will return 1
+ * @depend_on strlength()
+ * @param string The string where we are going to search for model.
+ * @param model The model we are using to search in string.
+ * @return The amount of char that is similar from the start of string.
+ */
+int count_matching_char(const char* string, const char* model);
+
+/**
+ * Use to find inside a string every start index occurrence of an model.
+ * @param string The string we are going to search in.
+ * @param model The model we are going to search for.
+ * @param amount_of_index The pointer of where we are going to store the amount of index found.
+ * @return A list of index that point to the start of the model inside the string.
+ */
+int* find_index(const char* string, const char* model, int* amount_of_index);
+
+/**
+ * Use to check if a string start with a certain model.
+ * @depend_on strlength()
+ * @param string The string we are going to check for.
+ * @param model The model to check for at the start of the string.
+ * @return 1 (True) if the string start with the model, 0 (False) if not.
+ */
+int start_with(const char* string, const char* model);
+
+/**
+ * Use to count a string inside another string.
+ * @depend_on strlength(), substring(), start_with(), free()
+ * @param string The string were we are going to count for 'to_count'
+ * @param model The string we want to count the amount of occurrence inside 'string'
+ * @return The amount of occurrence found inside 'string' of 'to_count'
+ */
+int count(const char* string, const char* model);
+
+/**
+ * Use to replace 'limit' occurrence of 'to_replace' by 'replacement' inside 'string'.
+ * @depend_on strlength(), max(), calloc(), substring(), count_matching_row_char(), free()
+ * @param string The string where we are going to replace things inside.
+ * @param to_replace The string we want to replace.
+ * @param replacement The string that is going to replace 'to_replace' inside 'string'
+ * @param limit The amount of time we want to replace an occurrence (negative means infinite)
+ * @return The string with all replacement done.
+ */
+char* replace(const char* string, const char* to_replace, const char* replacement, int limit);
+
+/**
+ * Use to replace the first occurrence of 'to_replace' by 'replacement' inside 'string'.
+ * @depend_on replace()
+ * @param string The string where we are going to replace things inside.
+ * @param to_replace The string we want to replace.
+ * @param replacement The string that is going to replace 'to_replace' inside 'string'
+ * @return The string with all replacement done.
+ */
+char* replace_first(const char* string, const char* to_replace, const char* replacement);
+
+/**
+ * Use to replace 'limit' occurrence of 'to_replace' by 'replacement' inside 'string' from the end of the string.
+ * @depend_on strlength(), max(), calloc(), substring(), count_matching_row_char(), free()
+ * @param string The string where we are going to replace things inside.
+ * @param to_replace The string we want to replace.
+ * @param replacement The string that is going to replace 'to_replace' inside 'string'
+ * @param limit The amount of time we want to replace an occurrence (negative means infinite)
+ * @return The string with all replacement done.
+ */
+char* replace_from_end(const char* string, const char* to_replace, const char* replacement, int limit);
+
+/**
+ * Use to replace the last occurrence of 'to_replace' by 'replacement' inside 'string' from the end of the string.
+ * @depend_on replace()
+ * @param string The string where we are going to replace things inside.
+ * @param to_replace The string we want to replace.
+ * @param replacement The string that is going to replace 'to_replace' inside 'string'
+ * @return The string with all replacement done.
+ */
+char* replace_last(const char* string, const char* to_replace, const char* replacement);
+
+/**
+ * Use to split a string in different string.
+ * @param string The string that is going to be split.
+ * @param splitter The string that will split the 'string'.
+ * @param limit The max amount of split object (negative or null value mean infinite split).
+ * @param string_amount A pointer of where we are going to write down the total of object inside the returned array.
+ * @return A pointer to the array of string.
+ */
+char** split(const char* string, const char* splitter, int limit, int* string_amount);
+
+#endif //ERIS_STRING_UTILS_H
